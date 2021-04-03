08bf614ed5984ea6f4cc1affb0ffd80d35a3b1fc
GetScoreSetScore <andy844551@gmail.com>
2021-04-04 02:25:02 +0300

    Add project files.

---
+++ laba/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace laba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.memee = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.memee,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(279, 102);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(570, 236);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // memee
+            // 
+            this.memee.Text = "MAC address";
+            this.memee.Width = 156;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "IP address";
+            this.columnHeader3.Width = 144;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Device name";
+            this.columnHeader4.Width = 170;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Thread id";
+            this.columnHeader1.Width = 95;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 102);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(273, 236);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(10, 42);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Resolve names via dns";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 410);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader memee;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ laba/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Windows.Forms.Design;
+using System.Threading;
+
+namespace laba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool resolveNames = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            richTextBox1.Clear();
+            NetworkInterface[] Interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            
+            resolveNames = checkBox1.Checked;
+            foreach (NetworkInterface Interface in Interfaces )
+            {
+                if (Interface.OperationalStatus != OperationalStatus.Up) continue;
+                if (Interface.GetIPProperties().GatewayAddresses.Any())
+                {
+                    richTextBox1.AppendText(Interface.GetPhysicalAddress().ToString() + "\n");
+                    if( resolveNames )
+                        richTextBox1.AppendText(System.Net.Dns.GetHostName() + "\n"); 
+                    richTextBox1.AppendText(Interface.Description.ToString() + "\n\n");
+                    UnicastIPAddressInformationCollection UnicastIPInfoCol = Interface.GetIPProperties().UnicastAddresses;
+
+                    foreach (UnicastIPAddressInformation UnicatIPInfo in UnicastIPInfoCol)
+                    {
+                        richTextBox1.AppendText("\tIP Address is" + UnicatIPInfo.Address.ToString() + "\n");
+                        richTextBox1.AppendText("\tSubnet Mask is" + UnicatIPInfo.IPv4Mask.ToString() + "\n");
+                        UInt32[] interval = GetInterval(UnicatIPInfo.Address.ToString(), UnicatIPInfo.IPv4Mask.ToString());
+                        richTextBox1.AppendText("\tstart" + UInt32ToIPAddress(interval[0]).ToString() + "\n");
+                        richTextBox1.AppendText("\tEnd" + UInt32ToIPAddress(interval[1]).ToString() + "\n");
+                        IPAddress adr;
+                        for (UInt32 i = interval[0]; i < interval[1]; i++) {
+                            adr = UInt32ToIPAddress(i);
+                            Ping p = new Ping();
+
+                            p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
+                            p.SendAsync(adr, 2000, adr);
+                        }
+
+                   } 
+                }
+            }
+
+        }
+        public static IPAddress UInt32ToIPAddress(UInt32 address)
+        {
+            return new IPAddress(new byte[] { 
+                (byte)((address>>24) & 0xFF) ,
+                (byte)((address>>16) & 0xFF) , 
+                (byte)((address>>8)  & 0xFF) , 
+                (byte)( address & 0xFF)});
+        }
+        private UInt32[] GetInterval(String adr, String mas)
+        {
+            UInt32 address = BitConverter.ToUInt32(IPAddress.Parse(adr).GetAddressBytes(), 0);
+            UInt32 mask = BitConverter.ToUInt32(IPAddress.Parse(mas).GetAddressBytes(), 0);
+            UInt32 start = (address & mask); 
+            byte[] star = BitConverter.GetBytes(start);
+            star[3]++; 
+            Array.Reverse(star);
+            start = BitConverter.ToUInt32(star, 0);
+            UInt32 end = (address | (~mask));
+            byte[] en = BitConverter.GetBytes(end);
+            en[3]--; 
+            Array.Reverse(en);
+            end = BitConverter.ToUInt32(en,0);
+            UInt32[] res=new UInt32[]{start,end};
+            return res;
+        }
+        public void p_PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            string ip = e.UserState.ToString();
+            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
+            {
+                
+                ListViewItem item = new ListViewItem(GetMacAddress(ip));
+                item.SubItems.Add(ip);
+                richTextBox1.AppendText(ip+" is up\n");
+
+                if (resolveNames)
+                {
+                    string name;
+                    try
+                    {
+                        IPHostEntry hostEntry = Dns.GetHostEntry(ip);
+                        name = hostEntry.HostName;
+                    }
+                    catch (Exception ex)
+                    {
+                        name = "?";
+                    }
+                    item.SubItems.Add(name);
+                }
+                else {
+                    item.SubItems.Add("?");
+                }
+                item.SubItems.Add(Thread.CurrentThread.ManagedThreadId.ToString());
+                listView1.Items.Add(item);
+            }
+            else if (e.Reply == null)
+            {
+                
+            }
+            ((Ping)sender).Dispose();
+
+        }
+        public static string GetMacAddress(string ipAddress)
+        {
+            var macAddress = string.Empty;
+            var pProcess = new System.Diagnostics.Process();
+
+            pProcess.StartInfo.FileName = "arp";
+            pProcess.StartInfo.Arguments = "-a " + ipAddress;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+
+            string strOutput = pProcess.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+
+            if (substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                         + "-" + substrings[7] + "-"
+                         + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+            else
+            {
+                return "not found";
+            }
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
